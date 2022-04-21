42bef131aff71d8dc710e0870b7cbd608fec25e9
kurucaymurat7 <100161987+kurucaymurat7@users.noreply.github.com>
2022-04-21 12:07:13 +0300

    depo yonetimi projesi

---
+++ src/depopackage/main.java
@@ -0,0 +1,22 @@
+package depopackage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static depopackage.Methods.*;
+
+public class main {
+    public static void main(String[] args) {
+
+        urunTanimla();
+
+        urunListele();
+
+        urunGirisi();
+
+        urunuRafaKoy();
+
+        urunCikisi();
+
+    }
+}
